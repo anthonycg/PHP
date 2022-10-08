b933b5bf3b8c3067fbf8610c4cc83b97a8fdab26
Anthony <anthonygibson24@gmail.com>
2022-10-08 18:59:55 -0500

    first commit

---
+++ Fundamentals/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Test</title>
+</head>
+
+<body>
+  <?php 
+  $first_name = "Anthony";
+  $last_name = "Gibson";
+  $occupation = "Software Engineer";
+
+  echo "My name is $first_name <br>";
+  echo "My last name is" . ''.  " $last_name," . '' . "and my occupation is $occupation";
+  ?>
+</body>
+</html> 
