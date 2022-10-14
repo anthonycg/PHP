5570d0ff2af758fca666f0169efac8f322a9ab55
Anthony <anthonygibson24@gmail.com>
2022-10-13 23:37:52 -0500

    learned about multidimensional arrays

--- Fundamentals/index.php
+++ Fundamentals/index.php
@@ -76,6 +76,24 @@
   ?>
 
 <?php
+$exams = array(
+  array(
+   'title' => 'Yellow Belt Exam',
+   'language' => 'HTML & CSS'
+  ),
+  array(
+   'title' => 'Red Belt Exam',
+   'language' => 'PHP using CodeIgniter'
+  ),
+  array(
+   'title' => 'Black Belt Exam',
+   'language' => 'Ruby using Ruby on Rails'
+  )
+ );
+ 
+ 
+
+
 $numbers = array(1, 2, 5, 10, 255, 3);
 $total = 0;
 foreach($numbers as $number) {
