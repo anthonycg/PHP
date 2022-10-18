0cfdc1a52be0ef327bb3bb8490aca9901ab8b342
Anthony <anthonygibson24@gmail.com>
2022-10-17 22:52:27 -0500

    average exercise done

--- Fundamentals/index.php
+++ Fundamentals/index.php
@@ -22,106 +22,17 @@ echo $lowercase;
 
 ?>
 
-  <?php 
-  $first_name = "Anthony";
-  $last_name = "Gibson";
-  $occupation = "Software Engineer";
-  $jobList = array();
-  $jobList[] = "civil engineer";
-  $jobList[] = "doctor";
-  $jobList[] = "pilot";
-
-  echo "My first name is $first_name <br>";
-  echo "My last name is $last_name" . ", and" . "I used to be a $jobList[0]. <br>";
-  echo "I'm currently a $occupation <br>";
-  ?>
-
-  <?php 
-  $party_people = array();
-  $party_people[] = array("name" => "Mike1", "age" => 21);
-  $party_people[] = array("name" => "Mike2", "age" => 21);
-  $party_people[] = array("name" => "Mike3", "age" => 234);
-  $party_people[] = array("name" => "Mike4", "age" => 210);
-  $party_people[] = array("name" => "Mike5", "age" => 21);
-  $party_people[] = array("name" => "Mike6", "age" => 20);
-  $party_people[] = array("name" => "Mike7", "age" => 21);
-  $party_people[] = array("name" => "Mike8", "age" => 20);
-  $party_people[] = array("name" => "Mike9", "age" => 21);
-  $party_people[] = array("name" => "Mike10", "age" => 21);
-  $party_people[] = array("name" => "Mike11", "age" => 20);
-  $party_people[] = array("name" => "Mike12", "age" => 21);
-  $party_people[] = array("name" => "Mike13", "age" => 20);
-  $party_people[] = array("name" => "Mike14", "age" => 21);
-  $party_people[] = array("name" => "Mike15", "age" => 21);
-  $party_people[] = array("name" => "Mike16", "age" => 21);
-  $party_people[] = array("name" => "Mike17", "age" => 21);
-  $party_people[] = array("name" => "Mike18", "age" => 21);
-  $party_people[] = array("name" => "Mike19", "age" => 20);
-
-  $capacity = 0;
-  $party_message = '';
-  $number_in_party = 0;
-  
-
-  foreach($party_people as $person) {
-    if ($party_people[$capacity]["name"] == "Mike3" || $party_people[$capacity]["name"] == "Mike2") {
-      $party_message = "Sorry" . $party_people[$capacity]["name"] . "You're not allowed. <br>";
-    }
-    else if ($party_people[$capacity]["age"] < 21) {
-      $party_message = "  You're too young, kid. You're only" . $party_people[$capacity]["age"] . "<br>";
-    }
-    else if ($number_in_party >= 10) {
-      $party_message = "Sorry, the party is full <br>";
-    }
-    else {
-      $party_message = "Welcome." . $party_people[$capacity]["name"] . "<br>";
-      $number_in_party++;
-    }
-    $capacity++;
-    echo $party_message;
-  };
-
-  ?>
-
-  <?php
-  for($i = 0; $i < 100;$i++) {
-    echo $i . "<br>";
-  };
-  ?>
-
 <?php
-$exams = array(
-  array(
-   'title' => 'Yellow Belt Exam',
-   'language' => 'HTML & CSS'
-  ),
-  array(
-   'title' => 'Red Belt Exam',
-   'language' => 'PHP using CodeIgniter'
-  ),
-  array(
-   'title' => 'Black Belt Exam',
-   'language' => 'Ruby using Ruby on Rails'
-  )
- );
- 
- 
-
-
- for($i=0;i < count($exams); $i++) {
-  echo $exams[$i]['title'];
- };
-
-$numbers = array(1, 2, 5, 10, 255, 3);
+$sample = array(10, 3, 5, 8, 4, 2, 1, 333); 
 $total = 0;
-foreach($numbers as $number) {
-  $total = $number + $total;
-}
-
-$arrayLength = count($numbers);
-echo $total/$arrayLength;
-
+$divide = count($sample);
+foreach($sample as $sam) {
+$total = $total + $sam;
+};
+echo $total/$divide;
 ?>
+
+
 
 </body>
 </html> 
